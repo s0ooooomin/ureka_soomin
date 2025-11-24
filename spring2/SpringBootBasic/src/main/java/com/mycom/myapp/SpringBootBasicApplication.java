9d34c0d26dadbc7aadbae947edc8ecea3773c061
s0ooooomin <soominseo.student@gmail.com>
2025-11-24 12:04:46 +0900

    day56 morning

--- spring2/SpringBootBasic/src/main/java/com/mycom/myapp/SpringBootBasicApplication.java
+++ spring2/SpringBootBasic/src/main/java/com/mycom/myapp/SpringBootBasicApplication.java
@@ -3,7 +3,7 @@ package com.mycom.myapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication // @EnableAutoConfiguration + @ComponentScan + @Configuration
 public class SpringBootBasicApplication {
 
 	public static void main(String[] args) {
